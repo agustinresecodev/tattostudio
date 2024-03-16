c0c0550727c3beb372874148d11f6d6a47487333
Agustin Reseco Alonso <149005541+agustinresecodev@users.noreply.github.com>
2024-03-16 13:00:10 +0100

    Authorize middleware implementation in user, jobdates and artist routes

--- src/routes/artists.routes.ts
+++ src/routes/artists.routes.ts
@@ -1,10 +1,12 @@
 import express,{Request, Response} from 'express';
 import { artistController } from '../controllers/artistController';
+import { authorizeMiddleware } from '../middlewares/authorize';
 
 const router = express.Router();
 
 /////////      ARTISTS ROUTES      //////////////////
-router.get('/',artistController.getAll);
+router.get('/',authorizeMiddleware(["Artist"]),artistController.getAll);
+router.post('/create',authorizeMiddleware(["Admin"]),artistController.create);
 
 
 export default router;
--- src/routes/jobdates.routes.ts
+++ src/routes/jobdates.routes.ts
@@ -2,30 +2,31 @@ import express from 'express';
 import { jobdateController } from '../controllers/jobdateController';
 import { de } from '@faker-js/faker';
 import { authMiddleware } from '../middlewares/authMiddleware';
+import { authorizeMiddleware } from '../middlewares/authorize';
 const router = express.Router();
 
 /////////      JOBDATES ROUTES      //////////////////
 
 
 //get all jobdates
-router.get('/',authMiddleware, jobdateController.getAll);
+router.get('/',authMiddleware,authorizeMiddleware(["Admin"]), jobdateController.getAll);
 
 //get jobdatebyid
-router.get('/:id',authMiddleware, jobdateController.getById);
+router.get('/:id',authMiddleware,authorizeMiddleware(["Admin"]), jobdateController.getById);
 
 //create jobdate
-router.post('/create',authMiddleware, jobdateController.create);
+router.post('/create',authMiddleware,authorizeMiddleware(["Artist"]), jobdateController.create);
 
 //edit jobdate
-router.put('/:id',authMiddleware, jobdateController.update);
+router.put('/:id',authMiddleware,authorizeMiddleware(["Artist"]), jobdateController.update);
 
 //delete date
-router.delete('/:id',authMiddleware, jobdateController.delete);
+router.delete('/:id',authMiddleware,authorizeMiddleware(["Client","Artist"]), jobdateController.delete);
 
 //get jobdates by client
-router.get('/client/jobdates',authMiddleware, jobdateController.getByLogedClient);
+router.get('/client/jobdates',authMiddleware,authorizeMiddleware(["Client"]), jobdateController.getByLogedClient);
 
 //get jobdates by artist
-router.get('/artist/jobdates',authMiddleware, jobdateController.getByLogedArtist);
+router.get('/artist/jobdates',authMiddleware, authorizeMiddleware(["Artist"]),jobdateController.getByLogedArtist);
 
 export default router;
